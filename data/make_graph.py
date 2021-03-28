be78ab2f53b88ba3fe26b14babd002f4a9e7984c
Björn Sundin <bjornsundin02@gmail.com>
2021-03-28 17:34:47 +0200

    Jobba på rapport och analys

--- data/make_graph.py
+++ data/make_graph.py
@@ -12,12 +12,12 @@ with open(file_name, newline='') as file:
             numpy.array(list(map(lambda x: float(x) if x else numpy.nan, x)))
         , zip(*reader)
     ))
-    # axes = list(map(list, zip(*map(
-    #     lambda x: map(float, x), 
-    #     filter(lambda x: any(x), reader)
-    # ))))
 
 x = axes[0]
+
+pyplot.rcParams.update({
+    "text.usetex": True
+})
 
 pyplot.figure(figsize=(12, 5))
 pyplot.hlines(0, x[0], x[-1], colors="#ccc", linestyles="dashed")
@@ -26,7 +26,9 @@ for i, y in enumerate(axes[1:]):
     mask = numpy.isfinite(y)
     pyplot.plot(x[mask], y[mask], label = display_name)
 pyplot.legend()
-pyplot.xlabel("tid [sekunder]")
-pyplot.ylabel("avvikelse [meter]")
+pyplot.xlabel("Spänning [V]")
+pyplot.ylabel("$\\frac{\lambda}{I}$ [rad$^{-1}$s$^{-1}$]")
+# pyplot.xlabel("tid [sekunder]")
+# pyplot.ylabel("avvikelse [meter]")
 pyplot.autoscale(tight=True)
 pyplot.savefig(f"../images/graf_{os.path.splitext(file_name)[0]}.png", bbox_inches="tight")
