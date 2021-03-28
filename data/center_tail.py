aae8beefee725a04e27a3146cd2944a9cfe2375e
Björn Sundin <bjornsundin02@gmail.com>
2021-03-28 22:54:26 +0200

    Fixa tidsaxlar och fortsätt med rapport

--- data/center_tail.py
+++ data/center_tail.py
@@ -10,10 +10,10 @@ with open(file_name, newline='') as file:
     name, extension = os.path.splitext(file_name)
     writer = csv.writer(open(f"{name}_centered{extension}", 'w', newline=''), delimiter='\t')
     for row in rows:
-        writer.writerow(map(
-            lambda x: 
-                x[1] if x[0] == 0 else 
-                    (str(float(x[1]) - float(rows[-1][x[0]])) if x[1] else x[1])
-            , enumerate(row)
-        ))
-
+        if any(row[1:]):
+            writer.writerow(map(
+                lambda x: 
+                    str(float(x[1]) - float(rows[0][0])) if x[0] == 0 else 
+                        (str(float(x[1]) - float(rows[-1][x[0]])) if x[1] else x[1])
+                , enumerate(row)
+            ))
